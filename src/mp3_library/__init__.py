d88c490cb8207e8e6a2fbeba5190a17c6d262630
MelvDouc <melv.douc@gmail.com>
2025-05-23 09:20:10 +0200

    fix: Template layout

--- src/mp3_library/__init__.py
+++ src/mp3_library/__init__.py
@@ -14,6 +14,7 @@ def create_app() -> Flask:
 
 
 def handle_exception(e):
+    print(e)
     response = make_response()
     response.data = json_stringify({
         "message": "Sorry, I must've messed up somewhere."
